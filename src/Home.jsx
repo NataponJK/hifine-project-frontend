e500d2e78db5e2e14afbecd57f63d337b3e5dbf2
nataponj <nataponj@gmail.com>
2024-02-01 19:47:10 +0700

    finish draft axios in Home and Profile

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostCard from "./UserComponents/PostCard";
 import PostModal from "./UserComponents/PostModal";
 import UserLayout from "./UserComponents/UserLayout";
+import axios from "axios";
 
 function Home() {
   const [openModal, setOpenModal] = useState(false);
-  
+  const [user, setUser] = useState([]);
+  const [posts, setPosts] = useState([])
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("Link");
+      setUser(response.data);
+    };
+    getData();
+  }, [reload]);
+
+  const createPost = async (
+    id,
+    name,
+    description,
+    distance,
+    duration,
+    type
+  ) => {
+    const requestPost = {
+      id: id,
+      name: name,
+      description: description,
+      distance: distance,
+      duration: duration,
+      type: type,
+    };
+    const response = await axios.post("Link", requestPost);
+    if (response.status === 200) {
+      setReload(!reload);
+    }
+  };
+
+  const editPost = async (id, name, description, distance, duration, type) => {
+    const requestPost = {
+      id: id,
+      name: name,
+      description: description,
+      distance: distance,
+      duration: duration,
+      type: type,
+    };
+    const response = await axios.put("Link");
+    if (response.status === 200) {
+      setReload(!reload);
+    }
+  };
+
+  const deletePost = async (id) => {
+    const post_id = id;
+    const response = await axios.delete("Link/${post_id}");
+    if (response.status === 200) {
+      setReload(!reload);
+    }
+  };
+
   return (
     <UserLayout>
       <div className="flex justify-center sm:w-[640px] mx-auto border-2 border-black rounded-xl mt-2">
@@ -18,8 +75,26 @@ function Home() {
           This is Modal Button
         </button>
       </div>
-      {openModal && <PostModal setOpenModal={setOpenModal} />}
-      <PostCard />
+      {openModal && (
+        <PostModal
+          setOpenModal={setOpenModal}
+          submitPost={createPost}
+          submitEdit={editPost}
+          submitDelete={deletePost}
+        />
+      )}
+      <div>
+        {posts.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            description={post.description}
+            distance={post.distance}
+            duration={post.duration}
+            type={post.type}
+          />
+        ))}
+      </div>
     </UserLayout>
   );
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,27 +1,37 @@
+import { useEffect, useState } from "react";
 import UserLayout from "./UserComponents/UserLayout";
 
 const Profile = () => {
+  const [user, setUser] = useState([]);
+  const [media, setMedia] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("Link/User");
+      setUser(response.data);
+    };
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      const response = await axios.get("Link/UserID/Media");
+      setMedia(response.data);
+    };
+    getMedia();
+  }, []);
+
   return (
     <UserLayout>
       <div className="flex flex-col sm:w-[640px] w-screen ml-auto mr-auto justify-center items-center rounded-lg bg-red-400 mt-2">
         <div className="w-[250px] h-[250px] mt-10 mb-10 bg-pink-600 rounded-full">
           <img />
+          <p>{user.displayName}</p>
         </div>
         <div className="flex flex-wrap w-fit justify-center items-center">
-          {/* {media.map((image)=> {
-            return (
-              <img className="w-1/3" alt="image" key={image.id}/>
-            )})
-          } */}
-          <img className="w-1/3 bg-black" alt="image"/>
-          <img className="w-1/3 bg-white" alt="image"/>
-          <img className="w-1/3 bg-black" alt="image"/>
-          <img className="w-1/3 bg-white" alt="image"/>
-          <img className="w-1/3 bg-black" alt="image"/>
-          <img className="w-1/3 bg-white" alt="image"/>
-          <img className="w-1/3 bg-black" alt="image"/>
-          <img className="w-1/3 bg-white" alt="image"/>
-          <img className="w-1/3 bg-black" alt="image"/>
+          {media.map((image) => {
+            return <img className="w-1/3" alt="image" key={image.id} />;
+          })}
         </div>
       </div>
     </UserLayout>
