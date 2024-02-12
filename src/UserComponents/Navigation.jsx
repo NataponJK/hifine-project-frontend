a1d7042b397d7fb8a6d336c83a65dcf551a2fdb1
nataponj <nataponj@gmail.com>
2024-02-12 14:00:41 +0700

    change nav bar navigation & modal to icon

--- src/UserComponents/Navigation.jsx
+++ src/UserComponents/Navigation.jsx
@@ -1,32 +1,61 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
+  const { pathname } = useLocation();
+  const subpage = pathname.split("/")?.[1];
+
+  const HighlightLink = (type = null) => {
+    let classes =
+      "flex justify-center w-full items-center rounded hover:bg-[#C0C0C0]";
+    if (type === subpage) {
+      classes += "text-white bg-[#A9A9A9]";
+    } else {
+      classes += " ";
+    }
+    return classes;
+  };
+
   return (
     <ul className="flex justify-evenly h-full">
-      <li className="flex w-1/3 bg-zinc-600 hover:bg-zinc-900">
-        <Link to="/Home" className="flex justify-center w-full">
+      <li className="flex w-1/3">
+        <Link to="/Home" className={HighlightLink("Home")}>
           <button className="flex justify-center items-center w-full rounded">
-            <span class="material-symbols-rounded" style={{ fontSize: 40 }}>
-              Home
-            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="40"
+              viewBox="0 -960 960 960"
+              width="40"
+            >
+              <path d="M240-200h120v-200q0-17 11.5-28.5T400-440h160q17 0 28.5 11.5T600-400v200h120v-360L480-740 240-560v360Zm-80 0v-360q0-19 8.5-36t23.5-28l240-180q21-16 48-16t48 16l240 180q15 11 23.5 28t8.5 36v360q0 33-23.5 56.5T720-120H560q-17 0-28.5-11.5T520-160v-200h-80v200q0 17-11.5 28.5T400-120H240q-33 0-56.5-23.5T160-200Zm320-270Z" />
+            </svg>
           </button>
         </Link>
       </li>
-      <li className="flex w-1/3 bg-zinc-300 hover:bg-zinc-900">
-        <Link to="/Profile" className="flex justify-center w-full">
+      <li className="flex w-1/3">
+        <Link to="/Profile" className={HighlightLink("Profile")}>
           <button className="flex justify-center items-center w-full rounded">
-            <span class="material-symbols-rounded" style={{ fontSize: 40 }}>
-              Account_circle
-            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="40"
+              viewBox="0 -960 960 960"
+              width="40"
+            >
+              <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+            </svg>
           </button>
         </Link>
       </li>
-      <li className="flex w-1/3 bg-zinc-600 hover:bg-zinc-900">
-        <Link to="/Lists" className="flex justify-center w-full">
+      <li className="flex w-1/3">
+        <Link to="/Lists" className={HighlightLink("Lists")}>
           <button className="flex justify-center items-center w-full rounded">
-            <span class="material-symbols-rounded" style={{ fontSize: 40 }}>
-              Format_list_bulleted
-            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="40"
+              viewBox="0 -960 960 960"
+              width="40"
+            >
+              <path d="M320-600q-17 0-28.5-11.5T280-640q0-17 11.5-28.5T320-680h480q17 0 28.5 11.5T840-640q0 17-11.5 28.5T800-600H320Zm0 160q-17 0-28.5-11.5T280-480q0-17 11.5-28.5T320-520h480q17 0 28.5 11.5T840-480q0 17-11.5 28.5T800-440H320Zm0 160q-17 0-28.5-11.5T280-320q0-17 11.5-28.5T320-360h480q17 0 28.5 11.5T840-320q0 17-11.5 28.5T800-280H320ZM160-600q-17 0-28.5-11.5T120-640q0-17 11.5-28.5T160-680q17 0 28.5 11.5T200-640q0 17-11.5 28.5T160-600Zm0 160q-17 0-28.5-11.5T120-480q0-17 11.5-28.5T160-520q17 0 28.5 11.5T200-480q0 17-11.5 28.5T160-440Zm0 160q-17 0-28.5-11.5T120-320q0-17 11.5-28.5T160-360q17 0 28.5 11.5T200-320q0 17-11.5 28.5T160-280Z" />
+            </svg>
           </button>
         </Link>
       </li>
--- src/UserComponents/PostModal.jsx
+++ src/UserComponents/PostModal.jsx
@@ -1,7 +1,6 @@
 import SelectActivityButton from "./SelectActivityButton/SelectActivityButton";
 
 const PostModal = ({ setOpenModal }) => {
-
   return (
     <div className="flex fixed top-0 left-0 w-full h-dvh justify-center items-center sm:mx-auto backdrop-blur-md">
       <div className="flex flex-col sm:w-[640px] mx-auto bg-white rounded-lg p-3">
@@ -11,9 +10,14 @@ const PostModal = ({ setOpenModal }) => {
             <h2>Create Post</h2>
           </div>
           <button onClick={() => setOpenModal(false)}>
-            <span class="material-symbols-rounded" style={{ fontSize: 40 }}>
-              close
-            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="40"
+              viewBox="0 -960 960 960"
+              width="40"
+            >
+              <path d="M480-424 284-228q-11 11-28 11t-28-11q-11-11-11-28t11-28l196-196-196-196q-11-11-11-28t11-28q11-11 28-11t28 11l196 196 196-196q11-11 28-11t28 11q11 11 11 28t-11 28L536-480l196 196q11 11 11 28t-11 28q-11 11-28 11t-28-11L480-424Z" />
+            </svg>
           </button>
         </div>
         <div className="p-2 border-black">
@@ -27,24 +31,35 @@ const PostModal = ({ setOpenModal }) => {
         <div className="modalFooter">
           <div className="flex flex-col">
             <div className="flex">
-              <div>
-              <p>Time</p>
-              <p>Distance</p>
+              <div className="flex gap-3">
+                <div className="flex gap-3">
+                  <p>Time</p>
+                  <input type="text" placeholder="Time" />
+                </div>
+                <div className="flex gap-3">
+                  <p>Distance</p>
+                  <input type="text" placeholder="Distance" />
+                </div>
               </div>
               <div>
                 <p></p>
               </div>
             </div>
             <div className="flex justify-between">
-            <div class="flex items-center gap-5">
-              <p>Add to Your Post</p>
-              <button>
-                <span class="material-symbols-rounded" style={{ fontSize: 40 }}>
-                  image
-                </span>
-              </button>
-            </div>
-            <SelectActivityButton />
+              <div class="flex items-center gap-5">
+                <p>Add Picture to Your Post</p>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="40"
+                    viewBox="0 -960 960 960"
+                    width="40"
+                  >
+                    <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0 0v-560 560Zm80-80h400q12 0 18-11t-2-21L586-459q-6-8-16-8t-16 8L450-320l-74-99q-6-8-16-8t-16 8l-80 107q-8 10-2 21t18 11Z" />
+                  </svg>
+                </button>
+              </div>
+              <SelectActivityButton />
             </div>
           </div>
           <div className="flex justify-center">
